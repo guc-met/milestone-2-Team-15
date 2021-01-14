abe0d4a009ef581cc4cac426fb3167000d4e6504
DoniaAli77 <donia.ali.7799@gmail.com>
2021-01-15 00:06:59 +0200

    nefsy a2fl

---
+++ milestone-2-team-15/src/components/StaffeditProfile.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react"
+import { Form, Row, Col, Button } from "react-bootstrap"
+import "../stylesheets/logout.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import backendlink from "../backendlink"
+import { useHistory } from "react-router-dom"
+export default function StaffeditProfile(props) {
+  const history = useHistory()
+  const link="/"+props.type+"/"+props.type+"profile"
+  
+
+  return (
+    <div className="logout">
+      <Col className="logoutButtonCol ">
+        <Button
+          variant="dark"
+          size="logoutButton"
+          type="submit"
+          onClick={ ()=>{history.push(link)}}
+        >
+          Profile
+        </Button>
+      </Col>
+    </div>
+  )
+}
